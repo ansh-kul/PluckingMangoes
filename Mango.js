bd008ff6d27f459ec9a9ec9651790eabff912722
ansh-kul <86955854+ansh-kul@users.noreply.github.com>
2021-08-10 18:58:35 +0530

    Add files via upload

---
+++ Mango.js
@@ -0,0 +1,24 @@
+class Mango{
+    constructor(x,y,diameter){
+        var options={
+            isStatic:true,
+            friction:2,
+            restitution:0
+        }
+        this.body = Bodies.rectangle(x,y,diameter/2,diameter/2,options);
+        this.diameter = diameter;
+        this.image = loadImage("Plucking mangoes/mango.png");
+        World.add(world,this.body);
+    }
+    display(){
+        var pos = this.body.position;
+      //  ellipseMode(CENTER);
+      //  ellipse(pos.x,pos.y,this.diameter);
+      push ()
+        translate (pos.x,pos.y);
+        rotate (this.body.angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.diameter,this.diameter);
+        pop ()
+    }
+}
